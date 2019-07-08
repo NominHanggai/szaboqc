1fbca32c54f1bbaa9813b5c49f85de79e04d4361
Mulliken <lux.qc@yandex.com>
2019-07-08 21:11:27 +0800

    GVB&MCSCF

--- heh.py
+++
@@ -1,6 +0,0 @@
-from pyscf import gto, scf
-heh = open('heh.dat','w')
-for i in range(1000):
-
-    e = scf.RHF(gto.M(atom='He 0 0 0; H 0 0 {}'.format(str(3.6*i/1000)),basis='sto-3g', charge=1, verbose=0, unit='bohr')).kernel()
-    print >>heh, str(3.5*i/1000), str(e)
--- heh2.py
+++
@@ -1,3 +0,0 @@
-from pyscf import gto, scf
-hf_scanner = scf.RHF(gto.Mole().set(verbose=0)).as_scanner()
-a = hf_scanner(gto.M(atom='H 0 0 0; F 0 0 1.1'))
